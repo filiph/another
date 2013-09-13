70a350df5ec051a39fdea27c8d00d2cb02a46c13
filiph <filip.hracek@gmail.com>
2013-09-13 23:54:41 +0200

    Improve config sample

--- config_sample.sh
+++ config_sample.sh
@@ -1,3 +1,5 @@
-# Change these according to your machine's setup.
+# Make a copy of this file, name it config.sh, and change these settings according to your
+# machine's setup.
+
 BLENDER_PATH="/opt/blender/blender"
 GIMP_PATH="/usr/bin/gimp"
