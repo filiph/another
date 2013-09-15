c0acf164cc160687f75787e37177e9db7f46c250
filiph <filip.hracek@gmail.com>
2013-09-15 22:56:47 +0200

    Add trolling detection

--- lib/vote_history_manager.py
+++ lib/vote_history_manager.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sqlite3
 
 import logging
@@ -7,6 +8,7 @@ class VoteHistoryManager:
     def __init__(self, filename="vote_history.db"):
         self.database_filename = filename
         self.no_emit_yet = True
+        self.latest_votes = deque([])
 
     def close(self):
         pass
@@ -20,7 +22,8 @@ class VoteHistoryManager:
                      "id INTEGER PRIMARY KEY, "
                      "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,"
                      "phenotype_id INTEGER,"
-                     "vote INTEGER);")
+                     "vote INTEGER,"
+                     "is_trolling BOOLEAN);")
         conn.commit()
         conn.close()
 
@@ -28,11 +31,31 @@ class VoteHistoryManager:
     NO = -1
     MEH = 0
 
-    def emit(self, phenotype, vote):
-        if self.no_emit_yet == True:
-            self._create_table_if_not_exists()
-        conn = self._create_connection()
-        conn.execute("INSERT INTO votes (phenotype_id, vote) VALUES (?, ?)", (phenotype.idn, vote))
-        conn.commit()
-        conn.close()
+    MAX_LATEST_VOTES = 5
 
+    def _detect_trolling(self):
+        if len(self.latest_votes) < self.MAX_LATEST_VOTES:
+            return False
+        all_same = all(x == self.latest_votes[0] for x in self.latest_votes)
+        if all_same and (self.latest_votes[0] == self.YES or self.latest_votes[0] == self.NO):
+            logger.info("Trolling detected (%s latest votes are the same).", self.MAX_LATEST_VOTES)
+            return True
+        else:
+            return False
+
+    def emit(self, phenotype, vote):
+        self.latest_votes.append(vote)
+        if len(self.latest_votes) > self.MAX_LATEST_VOTES:
+            self.latest_votes.popleft()
+        is_trolling = self._detect_trolling()
+        try:
+            if self.no_emit_yet == True:
+                self._create_table_if_not_exists()
+            conn = self._create_connection()
+            conn.execute("INSERT INTO votes (phenotype_id, vote, is_trolling) VALUES (?, ?, ?)",
+                         (phenotype.idn, vote, is_trolling))
+            conn.commit()
+            conn.close()
+        except sqlite3.Error as e:
+            logger.error("Record of vote not created, error with database: %s", e)
+        return is_trolling
